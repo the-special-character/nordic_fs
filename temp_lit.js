821e446b9a45e60e72cdfed5877f9a2762c8bda1
Mohammad Seraj <Qmdseraj@gmail.com>
2022-10-10 21:29:33 +0530

    template literal completed

---
+++ temp_lit.js
@@ -0,0 +1,14 @@
+const title ="Seraj_file"
+const html_Output =`<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title> ${title}</title>
+</head>
+<body>
+    <h1>Hello i'm Template literal</h1>
+</body>
+</html>`
+console.log(html_Output);
