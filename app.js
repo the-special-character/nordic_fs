d874e6f03b6837a9925524fe52f529e387a7e5ed
Yagnesh Modh <yagnesh.modh@gmail.com>
2022-11-11 21:29:47 +0530

    commit code

---
+++ app.js
@@ -0,0 +1,5 @@
+export function counter() {}
+
+// modules.exports = {
+//   counter,
+// };
---
+++ index.js
@@ -0,0 +1,12 @@
+// function add(a, b) {
+//     return a + b;
+// }
+
+// const counter = require("./app");
+import { counter } from "./app";
+
+const add = (a, b) => a + b;
+
+add();
+
+counter();
