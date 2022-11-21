1ea9f3fec58a201a4626cc16645126a33218453d
RITIK KUMAR KESHRI <keshri093@gmail.com>
2022-11-21 13:04:53 +0530

    todat 21/11

--- src/app.js
+++ src/app.js
@@ -1,7 +1,28 @@
-export function counter() {
-  return 'hello';
-}
+import React from 'react';
+// import App from './app';
+// first letter should be in upper case 
+// you can return only single Elementfrom component 
+// in line style  attribute is pass as an object 
+// insted of class use classs name 
 
-// modules.exports = {
-//   counter,
-// };
+
+function App({ name, degisination }) {
+  return (
+    <>
+      <h1
+        className="btn"
+        style={{
+          backgroungColor: 'yellow',
+          color: 'blue',
+        }}
+      >
+        {name}
+
+      </h1>
+      <h1>{degisination}</h1>
+      <div>
+        <input type="checkbox" />
+      </div>
+    </>
+  );
+}
