99ab9b50a91991e0bcfd56a141bb52411cfd60ed
Mohammad Seraj <Qmdseraj@gmail.com>
2022-11-24 01:27:12 +0530

    added rule: to avoid line break error

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,19 +3,14 @@ module.exports = {
     browser: true,
     es2021: true,
   },
-  extends: [
-    'plugin:react/recommended',
-    'airbnb',
-  ],
-  overrides: [
-  ],
+  extends: ['plugin:react/recommended', 'airbnb'],
+  overrides: [],
   parserOptions: {
     ecmaVersion: 'latest',
     sourceType: 'module',
   },
-  plugins: [
-    'react',
-  ],
+  plugins: ['react'],
   rules: {
+    'linebreak-style': 0,
   },
 };
