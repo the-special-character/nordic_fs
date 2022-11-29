0fd9d132962c7e9f71261459bf01b99d94774064
Yagnesh Modh <yagnesh.modh@gmail.com>
2022-11-29 20:44:31 +0530

    commit code

--- src/todo/index.jsx
+++ src/todo/index.jsx
@@ -1,9 +1,18 @@
 import React, { Component } from 'react';
 
+// create a component which has two child component
+// and each child component has click evnt.
+
+// if any error occurs during this click we have to handle this on parent component
+
 export default class Index extends Component {
   state = {};
 
   render() {
-    return <div>Index</div>;
+    return (
+      <div>
+        <h1 className="text-red-400 text-3xl">Index</h1>
+      </div>
+    );
   }
 }
--- webpack.config.js
+++ webpack.config.js
@@ -18,6 +18,11 @@ module.exports = {
         exclude: /node_modules/,
         use: 'babel-loader',
       },
+      {
+        test: /\.css$/,
+        exclude: /node_modules/,
+        use: ['style-loader', 'css-loader', 'postcss-loader'],
+      },
     ],
   },
   plugins: [
