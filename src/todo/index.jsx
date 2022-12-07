ebef5196273b4b2e8db96e41a9b81bc70beae7b0
Yagnesh Modh <yagnesh.modh@gmail.com>
2022-12-07 21:19:34 +0530

    commit code

--- src/todo/index.jsx
+++ src/todo/index.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component, createRef } from 'react';
 import './todo.css';
 
 // create a component which has two child component
@@ -7,58 +7,111 @@ import './todo.css';
 // if any error occurs during this click we have to handle this on parent component
 
 export default class Index extends Component {
-  state = { todoText: '', todoList: [] };
+  state = { todoList: [], filterType: 'all' };
 
-  changeTodoText = event => {
-    // console.log(event.target.value);
-    this.setState({ todoText: event.target.value });
+  intputTextRef = createRef();
+
+  filterBtns = [
+    {
+      name: 'All',
+      key: 'all',
+    },
+    {
+      name: 'Pending',
+      key: 'pending',
+    },
+    {
+      name: 'Completed',
+      key: 'completed',
+    },
+  ];
+
+  async componentDidMount() {
+    this.loadTodo();
+  }
+
+  loadTodo = async () => {
+    try {
+      const res = await fetch('http://localhost:3000/todoList');
+      const json = await res.json();
+      console.log(json);
+      this.setState({ todoList: json });
+    } catch (error) {}
   };
 
-  addTodo = event => {
-    event.preventDefault();
-    this.setState(({ todoList, todoText }) => ({
-      todoList: [
-        ...todoList,
-        { id: new Date().valueOf(), text: todoText, isDone: false },
-      ],
-      todoText: '',
-    }));
+  addTodo = async event => {
+    try {
+      event.preventDefault();
+
+      const res = await fetch('http://localhost:3000/todoList', {
+        method: 'POST',
+        body: JSON.stringify({
+          text: this.intputTextRef.current.value,
+          isDone: false,
+        }),
+        headers: {
+          'Content-Type': 'application/json',
+          Accept: 'application/json',
+        },
+      });
+
+      const json = await res.json();
+
+      this.setState(
+        ({ todoList }) => ({
+          todoList: [...todoList, json],
+        }),
+        () => {
+          this.intputTextRef.current.value = '';
+        }
+      );
+    } catch (error) {}
   };
 
-  toggleComplete = item => {
-    console.log(item);
-    // this.setState(({ todoList }) => ({
-    //   todoList: todoList.map(x => {
-    //     if (x.id === item.id) {
-    //       return { ...x, isDone: !x.isDone };
-    //     }
-    //     return x;
-    //   }),
-    // }));
+  toggleComplete = async item => {
+    try {
+      const res = await fetch(`http://localhost:3000/todoList/${item.id}`, {
+        method: 'PUT',
+        body: JSON.stringify({ ...item, isDone: !item.isDone }),
+        headers: {
+          'Content-Type': 'application/json',
+          Accept: 'application/json',
+        },
+      });
 
-    this.setState(({ todoList }) => {
-      const index = todoList.findIndex(x => x.id === item.id);
-      return {
-        todoList: [
-          ...todoList.slice(0, index),
-          { ...item, isDone: !item.isDone },
-          ...todoList.slice(index + 1),
-        ],
-      };
-    });
+      const json = await res.json();
+
+      this.setState(({ todoList }) => {
+        const index = todoList.findIndex(x => x.id === item.id);
+        return {
+          todoList: [
+            ...todoList.slice(0, index),
+            json,
+            ...todoList.slice(index + 1),
+          ],
+        };
+      });
+    } catch (error) {}
   };
 
-  deleteTodo = item => {
-    this.setState(({ todoList }) => {
-      const index = todoList.findIndex(x => x.id === item.id);
-      return {
-        todoList: [...todoList.slice(0, index), ...todoList.slice(index + 1)],
-      };
-    });
+  deleteTodo = async item => {
+    try {
+      await fetch(`http://localhost:3000/todoList/${item.id}`, {
+        method: 'DELETE',
+      });
+
+      this.setState(({ todoList }) => {
+        const index = todoList.findIndex(x => x.id === item.id);
+        return {
+          todoList: [...todoList.slice(0, index), ...todoList.slice(index + 1)],
+        };
+      });
+    } catch (error) {}
   };
 
   render() {
-    const { todoText, todoList } = this.state;
+    const { todoList, filterType } = this.state;
+    console.log('render');
     return (
       <div className="wrapper">
         <h1 className="heading">Todo App</h1>
@@ -66,42 +119,55 @@ export default class Index extends Component {
           <input
             type="text"
             className="rounded-l-md"
-            value={todoText}
-            onChange={this.changeTodoText}
+            ref={this.intputTextRef}
           />
           <button type="submit" className="btn rounded-l-none">
             Add Todo
           </button>
         </form>
         <div className="w-full flex-1">
-          {todoList.map(item => (
-            <div key={item.id} className="flex items-center m-4">
-              <input
-                type="checkbox"
-                checked={item.isDone}
-                onChange={() => this.toggleComplete(item)}
-              />
-              <p className="flex-1 px-8">{item.text}</p>
-              <button
-                type="button"
-                className="btn"
-                onClick={() => this.deleteTodo(item)}
-              >
-                Delete
-              </button>
-            </div>
-          ))}
+          {todoList
+            .filter(x => {
+              switch (filterType) {
+                case 'pending':
+                  return !x.isDone;
+
+                case 'completed':
+                  return x.isDone;
+
+                default:
+                  return true;
+              }
+            })
+            .map(item => (
+              <div key={item.id} className="flex items-center m-4">
+                <input
+                  type="checkbox"
+                  checked={item.isDone}
+                  onChange={() => this.toggleComplete(item)}
+                />
+                <p className="flex-1 px-8">{item.text}</p>
+                <button
+                  type="button"
+                  className="btn"
+                  onClick={() => this.deleteTodo(item)}
+                >
+                  Delete
+                </button>
+              </div>
+            ))}
         </div>
         <div className="w-full flex">
-          <button type="button" className="btn flex-1 rounded-none">
-            All
-          </button>
-          <button type="button" className="btn flex-1 rounded-none">
-            Pending
-          </button>
-          <button type="button" className="btn flex-1 rounded-none">
-            Completed
-          </button>
+          {this.filterBtns.map(x => (
+            <button
+              key={x.key}
+              type="button"
+              className="btn flex-1 rounded-none"
+              onClick={() => this.setState({ filterType: x.key })}
+            >
+              {x.name}
+            </button>
+          ))}
         </div>
       </div>
     );
--- webpack.config.js
+++ webpack.config.js
@@ -32,7 +32,7 @@ module.exports = {
     }),
   ],
   devServer: {
-    port: 3000,
+    port: 8080,
     open: true,
   },
 };
