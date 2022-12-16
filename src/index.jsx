abc9438eb2e465c537756d72556dd049701bea56
Yagnesh Modh <yagnesh.modh@gmail.com>
2022-12-16 21:06:48 +0530

    clearn branch

--- src/index.jsx
+++ src/index.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import Todo from './todo';
+import App from './app';
 import './main.css';
 
 const container = document.getElementById('root');
 const root = createRoot(container);
 
-root.render(<Todo />);
+root.render(<App />);
