ada3d0fd8b34dfbb1a7153a953a646b951d84410
Yagnesh Modh <yagnesh.modh@gmail.com>
2022-12-19 21:25:17 +0530

    commit code

--- src/index.jsx
+++ src/index.jsx
@@ -5,23 +5,45 @@ import {
   RouterProvider,
   Route,
   Link,
+  createRoutesFromElements,
 } from 'react-router-dom';
 import './main.css';
+import MainLayout from './layout/mainLayout';
+import Home from './pages/home';
+import About from './pages/about';
+import Login from './pages/login';
+import Register from './pages/register';
+import AuthLayout from './layout/authLayout';
 
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <h1>Home Page</h1>,
-  },
-  {
-    path: '/about',
-    element: <h1>About page</h1>,
-  },
-  {
-    path: '/contact',
-    element: <h1>Contact page</h1>,
-  },
-]);
+// const router = createBrowserRouter([
+//   {
+//     path: '/',
+//     element: <h1>Home Page</h1>,
+//   },
+//   {
+//     path: '/about',
+//     element: <h1>About page</h1>,
+//   },
+//   {
+//     path: '/contact',
+//     element: <h1>Contact page</h1>,
+//   },
+// ]);
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <>
+      <Route path="/" element={<MainLayout />}>
+        <Route index element={<Home />} />
+        <Route path="about" element={<About />} />
+      </Route>
+      <Route path="/auth" element={<AuthLayout />}>
+        <Route index element={<Login />} />
+        <Route path="register" element={<Register />} />
+      </Route>
+    </>
+  )
+);
 
 const container = document.getElementById('root');
 const root = createRoot(container);
