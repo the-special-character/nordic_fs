1b588da9e8d77099be28e119aca1c603aadd4f86
RITIK_KUMAR_KESHRIi <71667776+rkcoder093@users.noreply.github.com>
2022-12-20 21:03:40 +0530

    done twenty

---
+++ src/layout/authLayout.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import { Outlet } from 'react-router-dom';
+
+function AuthLayout() {
+  return (
+    <div className="flex min-h-full items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
+      <div className="w-full max-w-md space-y-8">
+        <div>
+          <img
+            className="mx-auto h-12 w-auto"
+            src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
+            alt="Your Company"
+          />
+          <h2 className="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900">
+            Sign in to your account
+          </h2>
+          <p className="mt-2 text-center text-sm text-gray-600">
+            Or{' '}
+            <a
+              href="/register"
+              className="font-medium text-indigo-600 hover:text-indigo-500"
+            >
+              start your 14-day free trial
+            </a>
+          </p>
+        </div>
+        <Outlet />
+      </div>
+    </div>
+  );
+}
+
+export default AuthLayout;
